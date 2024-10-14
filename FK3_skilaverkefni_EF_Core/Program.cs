783b53f53478d2ee678d9f72953dac06ff542e45
Óskar Eiðsson <ose1975@gmail.com>
2024-10-14 17:17:01 +0000

    First commit

--- FK3_skilaverkefni_EF_Core/Program.cs
+++ FK3_skilaverkefni_EF_Core/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using FK3_skilaverkefni_EF_Core.Models;
+using FK3_skilaverkefni_EF_Core.Data;
+
+namespace FK3_skilaverkefni_EF_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+        }
+    }
+}
